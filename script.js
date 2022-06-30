080cff6db52d6e611285908694afd5c412477d6d
Mark McNaughton <markusmack@hotmail.com>
2022-06-30 18:38:53 +0100

    added game logic, needs bug fixing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let GameBoard = (function(){
 
     let gameBoard = [];
+    const getGameBoard = () => gameBoard;
 
     for(let i = 0; i < 9; ++i) gameBoard.push(document.createElement('button'));
 
@@ -17,12 +18,14 @@ let GameBoard = (function(){
     }
 
     render();
+  
+    return {getGameBoard};
 
 })();
 
-let Player = (playerName) => {
+let Player = (playerName, playerIcon) => {
     let name = playerName;
-    let icon = ' ';
+    let icon = playerIcon;
 
     const getIcon = () => icon;
     const getName = () => name;
@@ -37,51 +40,144 @@ const PlayGame = (player1, player2, gameBoard) => {
     let p1 = player1;
     let p2 = player2;
     let board = gameBoard;
+    let currentPlayer = p1;
 
     // Cache DOM
     const iconX = document.querySelector('#x-icon');
     const iconO = document.querySelector('#o-icon');
+    const gameBoardBtns = document.querySelectorAll('.board-square');
+    const restartBtn = document.querySelector('.restart-btn');
 
     // Bind events
-    iconX.addEventListener('click', (e) => {
+    iconX.addEventListener('click', () => {
+        p1.setIcon('X');
+        p2.setIcon('O');
+        console.log(`Player 1 is ${p1.getIcon()}`);
+        console.log(`Player 2 is ${p2.getIcon()}`);
         iconX.classList.toggle('selected');
         if(iconO.classList.contains('selected')){
             iconO.classList.toggle('selected');
         }
-        iconSelect();
     });
-    iconO.addEventListener('click', (e) => {
+
+    iconO.addEventListener('click', () => {
+        p1.setIcon('O');
+        p2.setIcon('X');
+        console.log(`Player 1 is ${p1.getIcon()}`);
+        console.log(`Player 2 is ${p2.getIcon()}`);
         iconO.classList.toggle('selected');
         if(iconX.classList.contains('selected')){
             iconX.classList.toggle('selected');
         }
-        iconSelect();
     });
 
-    // Set the player's icon
-    const iconSelect = () => {
-        let icon = () => {
-            if(iconX.classList.contains('selected')){ return 'X';}
-            else if(iconO.classList.contains('selected')) { return 'O';}
-            else {return ' ';}
-        }
-        if(icon() === 'X') {
-            p1.setIcon(icon());
-            p2.setIcon('O');
-        } else if(icon() === 'O') {
-            p1.setIcon(icon());
-            p2.setIcon('X');
-        } else { alert('Choose an icon!'); }
+    gameBoardBtns.forEach(function(btn, index){
+        btn.addEventListener('click', () => {
+            iconX.disabled = true;
+            iconO.disabled = true;
+            if(btn.textContent === ' ') {
+                btn.textContent = currentPlayer.getIcon();
+                currentPlayer = (currentPlayer.getIcon() === 'X') ? p2 : p1;
+                hasWinner(currentPlayer);
+            }
+        });
+    });
 
-        console.log(`Player 1 is ${p1.getIcon()}`);
-        console.log(`Player 2 is ${p2.getIcon()}`);
+    restartBtn.addEventListener('click', resetGameBoard);
+
+    // Game logic
+    const hasWinner = (currentPlayer) => {
+        if(board.getGameBoard()[0].textContent === 'X' && board.getGameBoard()[1].textContent === 'X' && board.getGameBoard()[2].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[3].textContent === 'X' && board.getGameBoard()[4].textContent === 'X' && board.getGameBoard()[5].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[6].textContent === 'X' && board.getGameBoard()[7].textContent === 'X' && board.getGameBoard()[8].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[0].textContent === 'X' && board.getGameBoard()[3].textContent === 'X' && board.getGameBoard()[6].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[1].textContent === 'X' && board.getGameBoard()[4].textContent === 'X' && board.getGameBoard()[7].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[2].textContent === 'X' && board.getGameBoard()[5].textContent === 'X' && board.getGameBoard()[8].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[0].textContent === 'X' && board.getGameBoard()[4].textContent === 'X' && board.getGameBoard()[8].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[2].textContent === 'X' && board.getGameBoard()[4].textContent === 'X' && board.getGameBoard()[6].textContent === 'X') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+
+        // player 2
+        if(board.getGameBoard()[0].textContent === 'O' && board.getGameBoard()[1].textContent === 'O' && board.getGameBoard()[2].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[3].textContent === 'O' && board.getGameBoard()[4].textContent === 'O' && board.getGameBoard()[5].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[6].textContent === 'O' && board.getGameBoard()[7].textContent === 'O' && board.getGameBoard()[8].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[0].textContent === 'O' && board.getGameBoard()[3].textContent === 'O' && board.getGameBoard()[6].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[1].textContent === 'O' && board.getGameBoard()[4].textContent === 'O' && board.getGameBoard()[7].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[2].textContent === 'O' && board.getGameBoard()[5].textContent === 'O' && board.getGameBoard()[8].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[0].textContent === 'O' && board.getGameBoard()[4].textContent === 'O' && board.getGameBoard()[8].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
+        if(board.getGameBoard()[2].textContent === 'O' && board.getGameBoard()[4].textContent === 'O' && board.getGameBoard()[6].textContent === 'O') {
+            console.log(`${currentPlayer.getName()} WINS!`);
+            disableGameBoard();
+        };
     }
 
-    iconSelect();
-    
+    const disableGameBoard = () => {
+        gameBoardBtns.forEach(function(btn) {
+            btn.disabled = true;                
+        });
+    }
+
+    const enableGameBoard = () => {
+        gameBoardBtns.forEach(function(btn) {
+            btn.disabled = false;                
+        });
+    }
+
+    function resetGameBoard(){
+        gameBoardBtns.forEach(function(btn) {
+            btn.textContent = ' ';
+            iconX.disabled = false;
+            iconO.disabled = false;
+            enableGameBoard();           
+        }); 
+    }
 }
 
-const player1 = Player('Player 1');
-const player2 = Player('Player 2');
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
 
 PlayGame(player1, player2, GameBoard);
